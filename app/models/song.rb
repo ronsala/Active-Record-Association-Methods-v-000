63d66eb9b88bc63a469421a1e08e9574eff5caed
Ron Sala <ron.sala@gmail.com>
2018-06-15 02:47:13 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,12 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
-    # drake = Artist.where(name: "Drake") # []
-    # drake = Artist.find_by(name: "Drake")
-    # drake = Artist.find(1) # ActiveRecord::RecordNotFound: Couldn't find Artist with 'id'=1
     drake = Artist.create(:name=>'Drake')
-    # binding.pry
     self.artist = drake
   end
 end
